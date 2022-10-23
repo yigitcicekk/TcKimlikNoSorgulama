4ffbaba090b0e93c15f628259c9847041b91a78e
yigitcicekk <ygtcck.67@gmail.com>
2022-10-23 20:10:04 +0300

    TcKimlikNoSorgulama
    Kullanıcı tarafından girilen tc kimlik numarasını sistemde kayıtlı olan tc kimlik numarasıyla eşleşiyor mu kontrol eder

---
+++ TCKimlikNoSorgulama/TCKimlikNoSorgulama/Kimlik.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCKimlikNoSorgulama
+{
+    internal class Kimlik
+    {
+        private string _tckimliknumarasi;
+        public string TCKimliknumarasi
+        {
+            get { return _tckimliknumarasi.Substring(0, 3); }
+            set
+            {
+                if (value.Length == 11)
+                {
+                    bool bayrak = false;
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool karakterKontrol = char.IsNumber(value[i]);
+                        if (karakterKontrol)
+                        {
+                            // sayısal değer demektir...
+                        }
+                        else
+                        {
+                            bayrak = true;
+                            break;
+                        }
+                    }
+                    if (bayrak)
+                    {
+                        Console.WriteLine("Tc Kimlik numarası içindeki değerler sayısal olmalıdır ");
+                    }
+                    else
+                    {
+                        this._tckimliknumarasi = value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Tc Kimlik numarası 11 hane olmalıdır");
+                }
+            }
+        }
+    }
+}
---
+++ TCKimlikNoSorgulama/TCKimlikNoSorgulama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCKimlikNoSorgulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kimlik M1 = new Kimlik();
+
+            M1.TCKimliknumarasi = "12245678901";
+
+            string Tc = M1.TCKimliknumarasi;
+        }
+    }
+}
